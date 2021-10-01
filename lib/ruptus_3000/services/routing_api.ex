cce20c8f6c5f2237d15e50b8c41a9ab5dc4808c7
Samuel-88 <samuel.pereira@deliverycenter.com>
2021-10-01 17:14:48 -0300

    [DIII-10] float type added to distance in filter_response

--- lib/ruptus_3000/services/routing_api.ex
+++ lib/ruptus_3000/services/routing_api.ex
@@ -1,8 +1,18 @@
 defmodule Ruptus3000.Services.RoutingApi do
   alias Ruptus3000.Types.Coordinates
 
-  @callback request_route(Coordinates.t(), Coordinates.t()) :: {:ok, %{required(String.t()) => any()}} | {:error, String.t(), String.t()} | {:error, atom()}
+  @callback request_route(Coordinates.t(), Coordinates.t()) ::
+              {:ok, %{required(String.t()) => any()}}
+              | {:error, String.t(), String.t()}
+              | {:error, atom()}
 
-  @callback request_route(String.t(), String.t()) :: {:ok, %{required(String.t()) => any()}} | {:error, String.t(), String.t()} | {:error, atom()}
-  @callback filter_response(map()) :: %{distance: String.t() | integer(), duration: String.t() | integer(), polyline: String.t()}
+  @callback request_route(String.t(), String.t()) ::
+              {:ok, %{required(String.t()) => any()}}
+              | {:error, String.t(), String.t()}
+              | {:error, atom()}
+  @callback filter_response(map()) :: %{
+              distance: String.t() | integer() | float(),
+              duration: String.t() | integer(),
+              polyline: String.t()
+            }
 end
