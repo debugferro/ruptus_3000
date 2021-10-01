1cf307f03e38b43c232916abc191a1951cc20e70
Samuel-88 <samuel.pereira@deliverycenter.com>
2021-10-01 17:16:37 -0300

    [DIII-10] Response changed in render

--- lib/ruptus_3000_web/controllers/api/v1/delivery_controller.ex
+++ lib/ruptus_3000_web/controllers/api/v1/delivery_controller.ex
@@ -1,7 +1,10 @@
 defmodule Ruptus3000Web.Api.V1.DeliveryController do
   use Ruptus3000Web, :controller
 
-  def route(conn, _params) do
-    render(conn, "route.json", %{id: 1})
+  alias Ruptus3000.Driver.Selector
+
+  def route(conn, params) do
+    {:ok, _, response} = Selector.start(params)
+    render(conn, "route.json", %{result: response})
   end
 end
