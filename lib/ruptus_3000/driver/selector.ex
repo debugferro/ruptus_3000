1a564da679edae643c22cf477bb4b9a21e0a397e
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-04 17:38:49 -0300

    [DIII-14] Added CalcStraightPath handler.

--- lib/ruptus_3000/driver/selector.ex
+++ lib/ruptus_3000/driver/selector.ex
@@ -7,11 +7,13 @@ defmodule Ruptus3000.Driver.Selector do
 
   alias Ruptus3000.Driver.GetDeliveryPath
   alias Ruptus3000.Driver.SelectDriversDistance
+  alias Ruptus3000.Driver.CalcStraightPath
 
   @spec start(map) :: {:ok, map()}
   def start(delivery_data) do
     delivery_data
     |> GetDeliveryPath.handle()
     |> SelectDriversDistance.handle()
+    |> CalcStraightPath.handle()
   end
 end
