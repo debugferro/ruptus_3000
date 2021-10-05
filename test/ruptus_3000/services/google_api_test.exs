360a30d3cbca37310b267c645d2ee4882e246f1e
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-05 20:50:14 -0300

    [DIII-12] Changed tests descriptions.

--- test/ruptus_3000/services/google_api_test.exs
+++ test/ruptus_3000/services/google_api_test.exs
@@ -9,8 +9,8 @@ defmodule Ruptus3000.GoogleApiTest do
   @start_coordinates %{latitude: 33.81235029151803, longitude: -117.91837280484145}
   @end_coordinates %{latitude: 34.13824110217347, longitude: -118.3533032001647}
 
-  describe "GoogleApi.request_route/2 expects..." do
-    test "It to deal with a successful request for route" do
+  describe "GoogleApi.request_route/2" do
+    test "It deals with a successful request for route" do
       expect(HTTPoison, :get!, fn _, _, _ ->
         %{body: GoogleApiHelper.non_decoded_body(:successful), status_code: 200}
       end)
@@ -19,7 +19,7 @@ defmodule Ruptus3000.GoogleApiTest do
                GoogleApi.request_route(@start_coordinates, @end_coordinates)
     end
 
-    test "It to deal with a unsuccessful request for route" do
+    test "It deals with a unsuccessful request for route" do
       expect(HTTPoison, :get!, fn _, _, _ ->
         %{body: GoogleApiHelper.non_decoded_body(:unsuccessful), status_code: 200}
       end)
@@ -28,15 +28,15 @@ defmodule Ruptus3000.GoogleApiTest do
                GoogleApi.request_route(@start_coordinates, @end_coordinates)
     end
 
-    test "It to deal with unsuccessful status code" do
+    test "It deals with unsuccessful status code" do
       expect(HTTPoison, :get!, fn _, _, _ -> %{status_code: 404} end)
 
       assert {:error, :not_found} = GoogleApi.request_route(@start_coordinates, @end_coordinates)
     end
   end
 
-  describe "GoogleApi.filter_response/1 expects..." do
-    test "It to return a map with distance, duration and polyline" do
+  describe "GoogleApi.filter_response/1" do
+    test "It returns a map with distance, duration and polyline" do
       expect(Converter, :meters_to_km, fn 57811 -> 57.811 end)
 
       {:ok, response} = GoogleApiHelper.common_response()
