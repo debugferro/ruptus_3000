d98f6d4b950219f69fc59962f0dcfc115dacb386
Samuel-88 <samuel.pereira@deliverycenter.com>
2021-10-05 14:03:04 -0300

    [DIII-15] FilterDriversByRange module added

---
+++ lib/ruptus_3000/driver/filter_drivers_by_range.ex
@@ -0,0 +1,23 @@
+defmodule Ruptus3000.Driver.FilterDriversByRange do
+  @moduledoc """
+    This handler filter drivers by calculating each driver's collect distance (straight line),
+    adding the previously routed delivery distance and comparing it with the driver's vehicle
+    maximum allowed range.
+  """
+  @behaviour Ruptus3000.Driver.Handler
+
+  @spec handle({:ok, map(), map()}) ::
+          {:error, atom} | {:error, String.t(), atom()} | {:ok, map(), map()}
+  def handle({:ok, delivery_data, result}) do
+    drivers =
+      Enum.filter(result.delivery_people, fn driver ->
+        driver.straight_distance + result.to_delivery_point.distance <=
+          result.vehicles[driver["vehicle"]][:max_range]
+      end)
+
+    {:ok, delivery_data, Map.put(result, :delivery_people, drivers)}
+  end
+
+  def handle({:error, message, status}), do: {:error, message, status}
+  def handle({:error, status}), do: {:error, status}
+end
