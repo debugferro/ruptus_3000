41d2662628c96dd603e2a7d33d013304ad9d78a3
Samuel-88 <samuel.pereira@deliverycenter.com>
2021-10-06 10:06:08 -0300

    [DIII-15] module_doc and handle function updated

--- lib/ruptus_3000/driver/filter_drivers_by_range.ex
+++ lib/ruptus_3000/driver/filter_drivers_by_range.ex
@@ -1,7 +1,7 @@
 defmodule Ruptus3000.Driver.FilterDriversByRange do
   @moduledoc """
-    This handler filter drivers by calculating each driver's collect distance (straight line),
-    adding the previously routed delivery distance and comparing it with the driver's vehicle
+    This handler filter drivers by adding each driver's collect distance (straight line)
+    and the previously routed delivery distance, then comparing it with the driver's vehicle
     maximum allowed range.
   """
   @behaviour Ruptus3000.Driver.Handler
@@ -9,19 +9,21 @@ defmodule Ruptus3000.Driver.FilterDriversByRange do
   @spec handle({:ok, map(), map()}) ::
           {:error, atom} | {:error, String.t(), atom()} | {:ok, map(), map()}
   def handle({:ok, delivery_data, result}) do
-    drivers =
-      Enum.filter(result.delivery_people, fn driver ->
-        driver.straight_distance + result.to_delivery_point.distance <=
-          result.vehicles[driver["vehicle"]][:max_range]
-      end)
+    drivers = filter_drivers_by_range(result)
 
-    if drivers == [] do
-      {:error, "Não há entregadores disponíveis.", :no_delivery_person}
-    else
-      {:ok, delivery_data, Map.put(result, :delivery_people, drivers)}
+    case drivers do
+      [] -> {:error, "Não há entregadores disponíveis.", :no_delivery_person}
+      _ -> {:ok, delivery_data, Map.put(result, :delivery_people, drivers)}
     end
   end
 
   def handle({:error, message, status}), do: {:error, message, status}
   def handle({:error, status}), do: {:error, status}
+
+  defp filter_drivers_by_range(result) do
+    Enum.filter(result.delivery_people, fn driver ->
+      driver.straight_distance + result.to_delivery_point.distance <=
+        result.vehicles[driver["vehicle"]][:max_range]
+    end)
+  end
 end
