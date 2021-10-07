9db29ab2f65ce689bd3780b5da1ce948a6a0d1c5
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-06 22:33:20 -0300

    [DIII-25] Removed drivers by distance and time limits.

--- lib/ruptus_3000/driver/filter_drivers_by_range.ex
+++ lib/ruptus_3000/driver/filter_drivers_by_range.ex
@@ -10,7 +10,7 @@ defmodule Ruptus3000.Driver.FilterDriversByRange do
           {:error, atom} | {:error, String.t(), atom()} | {:ok, map(), map()}
   def handle({:ok, delivery_data, result}) do
 
-    case filter_drivers_by_range(result) do
+    case filter_drivers_by_range(result, delivery_data["max_delivery_time"]) do
       [] -> {:error, "Não há entregadores disponíveis.", :no_delivery_person}
       drivers -> {:ok, delivery_data, Map.put(result, :delivery_people, drivers)}
     end
@@ -19,7 +19,18 @@ defmodule Ruptus3000.Driver.FilterDriversByRange do
   def handle({:error, message, status}), do: {:error, message, status}
   def handle({:error, status}), do: {:error, status}
 
-  defp filter_drivers_by_range(result) do
+  defp filter_drivers_by_range(%{delivery_people: [%{to_collect_point: %{duration: _}} | _]} = result, max_time) do
+    Enum.filter(result.delivery_people, fn driver ->
+      driver.total_distance
+      <=
+      result.vehicles[driver["vehicle"]][:max_range]
+      and
+      driver.total_time
+      <= max_time
+    end)
+  end
+
+  defp filter_drivers_by_range(result, _max_delivery_time) do
     Enum.filter(result.delivery_people, fn driver ->
       driver.to_collect_point.distance + result.to_delivery_point.distance <=
         result.vehicles[driver["vehicle"]][:max_range]
