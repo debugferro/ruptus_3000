748fa2b8d8e335f96b8e5687cc9b39b5371c8f96
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-07 18:38:10 -0300

    [DIII-19] Renamed handler behaviour.

--- lib/ruptus_3000/driver/handler.ex
+++
@@ -1,6 +0,0 @@
-defmodule Ruptus3000.Driver.Handler do
-  alias Ruptus3000.Types.Error
-  @callback handle({:ok, map()}) :: {:ok, map()}
-  @callback handle(map() | {:ok, map(), map()}) :: {:ok, map(), map()} | Error.detailed_tuple() | Error.basic_tuple()
-  @callback handle(Error.detailed_tuple() | Error.basic_tuple()) :: Error.detailed_tuple() | Error.basic_tuple()
-end
