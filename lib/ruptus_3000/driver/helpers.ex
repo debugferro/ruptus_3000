24d00531637b3fa821683d6e134f0acc5d182a31
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-06 22:32:03 -0300

    [DIII-25] Added function to convert seconds to minutes.

--- lib/ruptus_3000/driver/helpers.ex
+++ lib/ruptus_3000/driver/helpers.ex
@@ -3,6 +3,8 @@ defmodule Ruptus3000.Driver.Helpers do
     distance / 1000
   end
 
+  def seconds_to_minutes(seconds), do: seconds / 60
+
   def localization_map(localization) do
     %{latitude: localization["latitude"], longitude: localization["longitude"]}
   end
