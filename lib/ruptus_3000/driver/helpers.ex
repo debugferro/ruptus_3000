7a95dcc863b10c127eb9aa5d689d4f2852eb65cb
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-07 16:20:17 -0300

    [DIII-25] Added spec to functions.

--- lib/ruptus_3000/driver/helpers.ex
+++ lib/ruptus_3000/driver/helpers.ex
@@ -1,10 +1,13 @@
 defmodule Ruptus3000.Driver.Helpers do
+  @spec meters_to_km(number) :: float
   def meters_to_km(distance) do
     distance / 1000
   end
 
+  @spec seconds_to_minutes(number) :: float
   def seconds_to_minutes(seconds), do: seconds / 60
 
+  @spec localization_map(%{:latitude => any(), :longitude => any()}) :: %{latitude: any, longitude: any}
   def localization_map(localization) do
     %{latitude: localization["latitude"], longitude: localization["longitude"]}
   end
