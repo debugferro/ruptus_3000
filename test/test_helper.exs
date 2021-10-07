ff35c995c63580248ff46cfdd83524dee9217a74
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-07 18:01:31 -0300

    [DIII-22] Mimic vehicle module.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,7 @@
 Mimic.copy(Ruptus3000.Services.GoogleApi)
 Mimic.copy(HTTPoison)
-Mimic.copy(Ruptus3000.Vehicle.Converter)
+Mimic.copy(Ruptus3000.Driver.Helpers)
+Mimic.copy(Ruptus3000.Vehicle)
 
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Ruptus3000.Repo, :manual)
