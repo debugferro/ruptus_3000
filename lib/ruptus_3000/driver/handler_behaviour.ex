d29a21dfcc5762354d3a174d1cb4bc54ce4062b4
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-09 01:28:51 -0300

    [DIII-27] Refactored behaviour expectations.

--- lib/ruptus_3000/driver/handler_behaviour.ex
+++ lib/ruptus_3000/driver/handler_behaviour.ex
@@ -1,8 +1,8 @@
 defmodule Ruptus3000.Driver.HandlerBehaviour do
   alias Ruptus3000.Types.Error
   @callback handle({:ok, map()}) :: {:ok, map()}
-  @callback handle(map() | {:ok, map(), map()}) ::
-              {:ok, map(), map()} | Error.detailed_tuple() | Error.basic_tuple()
+  @callback handle(map() | {:ok, map(), map()} | {:ok, map(), map(), map()}) ::
+              {:ok, map(), map()} | Error.detailed_tuple() | Error.basic_tuple() | {:ok, map(), map(), map()}
   @callback handle(Error.detailed_tuple() | Error.basic_tuple()) ::
               Error.detailed_tuple() | Error.basic_tuple()
 end
