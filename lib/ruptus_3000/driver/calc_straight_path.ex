5128582a00312e609fd6b4568a9166e69484b2f7
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-09 01:30:14 -0300

    [DIII-27] Refactored to calc a single straight path driver.

--- lib/ruptus_3000/driver/calc_straight_path.ex
+++ lib/ruptus_3000/driver/calc_straight_path.ex
@@ -9,29 +9,24 @@ defmodule Ruptus3000.Driver.CalcStraightPath do
   alias Ruptus3000.Driver.Helpers
   alias Ruptus3000.Types.Error
 
-  @spec handle({:ok, map(), map()} | Error.basic_tuple() | Error.detailed_tuple()) ::
-          Error.basic_tuple() | Error.detailed_tuple() | {:ok, map(), map()}
-  def handle({:ok, delivery_data, result}) do
-    drivers = calculate(result.drivers, delivery_data)
-    {:ok, delivery_data, build_return(result, drivers)}
+  @spec handle({:ok, map(), map(), map()} | Error.basic_tuple() | Error.detailed_tuple()) ::
+          Error.basic_tuple() | Error.detailed_tuple() | {:ok, map(), map(), map()}
+  def handle({:ok, driver, delivery_data, result}) do
+    distance = calculate(driver["localization"], delivery_data["collect_point"]["localization"])
+    {:ok, build_driver(driver, distance), delivery_data, result}
   end
 
   def handle({:error, message, status}), do: {:error, message, status}
   def handle({:error, status}), do: {:error, status}
 
-  defp calculate(drivers, delivery_data) do
-    Enum.map(drivers, fn driver ->
-      distance =
-        Haversine.distance(
-          build_localization(driver["localization"]),
-          build_localization(delivery_data["collect_point"]["localization"])
-        )
-        |> Helpers.meters_to_km()
-
-      Map.put(driver, :to_collect_point, %{distance: distance})
-    end)
+  defp calculate(driver_localization, collect_point_localization) do
+    Haversine.distance(
+      build_localization(driver_localization),
+      build_localization(collect_point_localization)
+    )
+    |> Helpers.meters_to_km()
   end
 
-  defp build_return(result, drivers), do: Map.put(result, :drivers, drivers)
+  defp build_driver(driver, distance), do: Map.put(driver, :to_collect_point, %{distance: distance})
   defp build_localization(localization), do: {localization["longitude"], localization["latitude"]}
 end
