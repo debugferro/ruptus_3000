7948ff3c6d28dccd8ca7a32aa74f13b4115d8a20
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-09 01:29:33 -0300

    [DIII-27] Refactored to deal with single driver request.

--- lib/ruptus_3000/driver/get_drivers_routes.ex
+++ lib/ruptus_3000/driver/get_drivers_routes.ex
@@ -8,26 +8,16 @@ defmodule Ruptus3000.Driver.GetDriversRoutes do
   alias Ruptus3000.Driver.Helpers
   alias Ruptus3000.Types.Error
 
-  @spec handle({:ok, map(), map()} | Error.basic_tuple() | Error.detailed_tuple()) ::
-          Error.basic_tuple() | Error.detailed_tuple() | {:ok, map(), map()}
-  def handle({:ok, delivery_data, result}) do
-    case start_async_requests(result, delivery_data) do
-      {:ok, drivers} -> {:ok, delivery_data, Map.put(result, :drivers, drivers)}
-      {:error, message, status} -> {:error, message, status}
+  @spec handle({:ok, map(), map(), map()} | Error.basic_tuple() | Error.detailed_tuple()) ::
+          Error.basic_tuple() | Error.detailed_tuple() | {:ok, map(), map(), map()}
+  def handle({:ok, driver, delivery_data, result}) do
+    case request_api(driver, delivery_data["collect_point"]["localization"], result) do
+      {:ok, driver} -> {:ok, driver, delivery_data, result}
+      error -> error
     end
   end
 
   def handle(error), do: error
-
-  defp start_async_requests(result, delivery_data) do
-    result.drivers
-    |> Task.async_stream(&request_api(&1, delivery_data["collect_point"]["localization"], result),
-      on_timeout: :kill_task,
-      ordered: false
-    )
-    |> Enum.map(&handle_info(&1))
-    |> check_for_errors()
-  end
 
   defp request_api(driver, collect_point, result) do
     case GoogleApi.request_route(
@@ -35,10 +25,9 @@ defmodule Ruptus3000.Driver.GetDriversRoutes do
            Helpers.localization_map(collect_point)
          ) do
       {:ok, response} ->
-        build_driver_info(driver, response |> GoogleApi.filter_response(), result)
+        {:ok, build_driver_info(driver, response |> GoogleApi.filter_response(), result)}
 
-      {:error, status} ->
-        Map.put(driver, :to_collect_point, %{error: status})
+      error -> error
     end
   end
 
@@ -47,12 +36,4 @@ defmodule Ruptus3000.Driver.GetDriversRoutes do
     |> Map.put(:total_time, response.duration + result.to_delivery_point.duration)
     |> Map.put(:total_distance, response.distance + result.to_delivery_point.distance)
   end
-
-  defp handle_info({:ok, result}), do: result
-
-  defp handle_info({:exit, :timeout}),
-    do: {:error, "Request timeout", :timeout_get_drivers_routes}
-
-  defp check_for_errors([{:error, message, status} | _]), do: {:error, message, status}
-  defp check_for_errors(result), do: {:ok, result}
 end
