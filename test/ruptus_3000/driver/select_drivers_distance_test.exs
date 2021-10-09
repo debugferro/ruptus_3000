575f6dd8894c31e2a7a8c72c9259071ab406e2aa
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-09 01:26:18 -0300

    [DIII-27] Changed tests to expect to check only a driver and not a list of drivers.

--- test/ruptus_3000/driver/select_drivers_distance_test.exs
+++ test/ruptus_3000/driver/select_drivers_distance_test.exs
@@ -4,50 +4,31 @@ defmodule Ruptus3000.Driver.SelectDriversDistanceTest do
 
   alias Ruptus3000.{Driver.SelectDriversDistance, Vehicle, HandlersHelpers}
 
-  @delivery_data %{
-    "delivery_people" => [%{
-      "id" => 1,
-      "vehicle" => "bike"
-    },
-    %{
-      "id" => 2,
-      "vehicle" => "motorcycle"
-    }]
-  }
-
   @result %{
-    to_delivery_point: %{
-      distance: 4.0
+    vehicles: %{
+      label_list: ["motorcycle"]
     }
   }
 
-  setup do
-    :ok = Ecto.Adapters.SQL.Sandbox.checkout(Ruptus3000.Repo)
-  end
+  @allowed_driver %{
+    "id" => 2,
+    "vehicle" => "motorcycle"
+  }
+
+  @not_allowed_driver %{
+    "id" => 2,
+    "vehicle" => "bike"
+  }
 
   describe "SelectDriversDistance handler" do
-    test "It returns vehicles and filtered delivery people in results" do
-      Vehicle
-      |> expect(:get_vehicles_by_max_range, fn _ -> HandlersHelpers.vehicle_query() end)
-      result = SelectDriversDistance.handle({:ok, @delivery_data, @result})
+    test "If a driver possesses an allowed vehicle it will return a successful response" do
+      result = SelectDriversDistance.handle({:ok, @allowed_driver, %{}, @result})
 
-      assert {:ok, @delivery_data, %{
-        vehicles: %{
-          "motorcycle" => %{
-            max_range: _,
-            priority_range_start: _,
-            priority_range_end: _
-          }
-        },
-        delivery_people: [%{
-          "id" => 2,
-          "vehicle" => "motorcycle"
-        }]
-      }} = result
+      assert {:ok, @allowed_driver, %{}, @result} = result
     end
 
-    test "It returns error when there is no drivers available"  do
-      result = SelectDriversDistance.handle({:ok, %{"delivery_people" => []}, @result})
+    test "It returns error when the driver possesses a not listed vehicle"  do
+      result = SelectDriversDistance.handle({:ok, @not_allowed_driver, %{}, @result})
 
       assert {:error, _message, _status} = result
     end
