59920ca8dbee14dc7a1e4919907396416fa5fb0c
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-13 12:30:04 -0300

    [DIII-27] Refactored test for loop refactoring.

---
+++ test/ruptus_3000/routing/get_delivery_path_test.exs
@@ -0,0 +1,57 @@
+defmodule Ruptus3000.Routing.GetDeliveryPathTest do
+  use ExUnit.Case, async: true
+  use Mimic
+
+  alias Ruptus3000.Routing.GetDeliveryPath
+  alias Ruptus3000.Services.GoogleApi
+  alias Ruptus3000.GoogleApiHelper
+
+  @data %{
+    "collect_point" => %{
+      "localization" => %{
+        "latitude" => 33.81235029151803,
+        "longitude" => -117.91837280484145
+      }
+    },
+    "delivery_point" => %{
+      "localization" => %{
+        "latitude" => 34.13824110217347,
+        "longitude" => -118.3533032001647
+      }
+    }
+  }
+
+  setup_all do
+    GoogleApi
+    |> expect(:request_route, fn _start_point, _end_point -> GoogleApiHelper.common_response() end)
+    |> expect(:filter_response, fn _response -> GoogleApiHelper.filter_response() end)
+
+    [result: GetDeliveryPath.handle(@data)]
+  end
+
+  describe "GetDeliveryPath handler" do
+    test "It returns a successful response", %{result: result} do
+      assert {:ok, @data, _} = result
+    end
+
+    test "It returns a complete unsuccessful response" do
+      GoogleApi
+      |> expect(:request_route, fn _, _ -> {:error, "error", "status"} end)
+
+      assert {:error, "error", "status"} = GetDeliveryPath.handle(@data)
+    end
+
+    test "It returns a basic unsuccessful response" do
+      GoogleApi
+      |> expect(:request_route, fn _, _ -> {:error, "status"} end)
+
+      assert {:error, "status"} = GetDeliveryPath.handle(@data)
+    end
+
+    test "It returns a map with the parsed response", %{result: result} do
+      {_, _, result_info} = result
+      assert %{to_delivery_point: %{ distance: _, duration: _, polyline: _}} = result_info
+      assert %{to_delivery_point: GoogleApiHelper.filter_response()} == result_info
+    end
+  end
+end
