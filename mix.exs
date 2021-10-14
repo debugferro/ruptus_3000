597b75caf9c1475093937f9bc78e73a2e3dd0db7
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-14 20:10:29 -0300

    [DIII] Added ex_json_schema lib.

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Ruptus3000.MixProject do
       {:haversine, "~> 0.1.0"},
       {:mimic, "~> 1.5", only: :test},
       {:flow, "~> 1.0"},
-      {:polyline, "~> 1.3"}
+      {:polyline, "~> 1.3"},
+      {:ex_json_schema, "~> 0.9.0"}
     ]
   end
 
