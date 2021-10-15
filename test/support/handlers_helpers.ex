acb67947569376fabd6c2c3037d644190561f36d
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-14 23:21:20 -0300

    [DIII-36] Added new helpers. Removed non used helpers.

--- test/support/handlers_helpers.ex
+++ test/support/handlers_helpers.ex
@@ -1,13 +1,20 @@
 defmodule Ruptus3000.HandlersHelpers do
 
-  @vehicle_query [
-    %{
-      label: "motorcycle",
-      max_range: 30.0,
-      priority_range_end: 10.0,
-      priority_range_start: 2.0,
-    }
-  ]
+  @motorcycle %{
+    label: "motorcycle",
+    max_range: 30.0,
+    priority_range_end: 30.0,
+    priority_range_start: 2.1,
+  }
+
+  @bike %{
+    label: "bike",
+    max_range: 2,
+    priority_range_start: 0,
+    priority_range_end: 2
+  }
+
+  @multiple_vehicles_query [@bike, @motorcycle]
 
   @result %{
     to_delivery_point: %{
@@ -16,6 +23,32 @@ defmodule Ruptus3000.HandlersHelpers do
     }
   }
 
-  def vehicle_query(), do: @vehicle_query
+  @basic_biker %{
+    "vehicle" => "bike",
+    to_collect_point: %{distance: 1}
+  }
+
+  @basic_motorcycle %{
+    "vehicle" => "motorcycle",
+    to_collect_point: %{distance: 1},
+  }
+
+  @vehicle_list %{
+    "bike" => %{
+      max_range: 2,
+      priority_range_start: 0,
+      priority_range_end: 2
+    },
+    "motorcycle" => %{
+      max_range: 30,
+      priority_range_start: 2.1,
+      priority_range_end: 30
+    }
+  }
+
+  def multiple_vehicles_query(), do: @multiple_vehicles_query
   def fake_result(), do: @result
+  def vehicle_list(), do: @vehicle_list
+  def basic_motorcycle(), do: @basic_motorcycle
+  def basic_biker(), do: @basic_biker
 end
