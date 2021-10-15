21dca338030d87f309f8b8f68933f3cd6535083f
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-14 21:31:35 -0300

    [DIII] Added error testing. Separeted all tests by description, and fixed helper alias.

--- test/ruptus_3000/routing/get_delivery_path_test.exs
+++ test/ruptus_3000/routing/get_delivery_path_test.exs
@@ -4,7 +4,13 @@ defmodule Ruptus3000.Routing.GetDeliveryPathTest do
 
   alias Ruptus3000.Routing.GetDeliveryPath
   alias Ruptus3000.Services.GoogleApi
+  alias Ruptus3000.Routing.Helpers
   alias Ruptus3000.GoogleApiHelper
+
+  @localization %{
+    "latitude" => 34.13824110217347,
+    "longitude" => -118.3533032001647
+  }
 
   @data %{
     "collect_point" => %{
@@ -14,26 +20,34 @@ defmodule Ruptus3000.Routing.GetDeliveryPathTest do
       }
     },
     "delivery_point" => %{
-      "localization" => %{
-        "latitude" => 34.13824110217347,
-        "longitude" => -118.3533032001647
-      }
+      "localization" => @localization
     }
   }
 
-  setup_all do
-    GoogleApi
-    |> expect(:request_route, fn _start_point, _end_point -> GoogleApiHelper.common_response() end)
-    |> expect(:filter_response, fn _response -> GoogleApiHelper.filter_response() end)
+  describe "GetDeliveryPath handler on successful responses" do
+    setup do
+      GoogleApi
+      |> expect(:request_route, fn _, _ -> GoogleApiHelper.common_response() end)
+      |> expect(:filter_response, fn _ -> GoogleApiHelper.filter_response() end)
 
-    [result: GetDeliveryPath.handle(@data)]
-  end
+      Helpers
+      |> expect(:get_localization, fn _, _ -> {:ok, @localization} end)
 
-  describe "GetDeliveryPath handler" do
+      [result: GetDeliveryPath.handle(@data)]
+    end
+
     test "It returns a successful response", %{result: result} do
       assert {:ok, @data, _} = result
     end
 
+    test "It returns a map with the parsed response", %{result: result} do
+      {_, _, result_info} = result
+      assert %{to_delivery_point: %{ distance: _, duration: _, polyline: _}} = result_info
+      assert %{to_delivery_point: GoogleApiHelper.filter_response()} == result_info
+    end
+  end
+
+  describe "GetDeliveryPath handler on unsuccessful api responses" do
     test "It returns a complete unsuccessful response" do
       GoogleApi
       |> expect(:request_route, fn _, _ -> {:error, "error", "status"} end)
@@ -47,11 +61,13 @@ defmodule Ruptus3000.Routing.GetDeliveryPathTest do
 
       assert {:error, "status"} = GetDeliveryPath.handle(@data)
     end
+  end
 
-    test "It returns a map with the parsed response", %{result: result} do
-      {_, _, result_info} = result
-      assert %{to_delivery_point: %{ distance: _, duration: _, polyline: _}} = result_info
-      assert %{to_delivery_point: GoogleApiHelper.filter_response()} == result_info
+  describe "GetDeliveryPath handler on invalid payload" do
+    test "It returns an error when localization is invalid" do
+      Helpers
+      |> expect(:get_localization, fn _, _ -> {:not_valid, "collect_point"} end)
+      assert {:error, _msg, :invalid_payload} = GetDeliveryPath.handle(@data)
     end
   end
 end
