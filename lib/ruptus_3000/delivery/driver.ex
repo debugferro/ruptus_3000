75d580e973c07a6d847fe0286cc54c54ff60646b
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-18 20:53:19 -0300

    [DIII-46]  Created driver schema.

---
+++ lib/ruptus_3000/delivery/driver.ex
@@ -0,0 +1,18 @@
+defmodule Ruptus3000.Delivery.Driver do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "drivers" do
+    field :external_id, :integer
+    field :full_name, :string
+    has_many :shipments, Ruptus3000.Delivery.Shipment
+    timestamps()
+  end
+
+  @doc false
+  def changeset(driver, attrs) do
+    driver
+    |> cast(attrs, [:external_id, :full_name])
+    |> validate_required([:external_id, :full_name])
+  end
+end
