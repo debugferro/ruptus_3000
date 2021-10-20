67d3decbc4d437873ceda9a9c4df2bc32b771936
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-20 13:28:32 -0300

    [DIII-47] Added select report hook event.

--- assets/js/select.js
+++ assets/js/select.js
@@ -1,18 +1,14 @@
-import { addPolyline } from './map'
-
 export const SelectReport = {
   mounted() {
     const hook = this;
     const selector = "#" + this.el.id;
     const element = this.el;
-    // console.log(this.el, this.el.id);
 
     element.addEventListener("click", evt => {
+      const reportId = element.getAttribute("report-id")
       hook.pushEventTo(selector, "select_report", {
-        report: element.getAttribute("report-id")
+        report_id: reportId
       })
-      polyline = document.getElementById(`info-${element.getAttribute("report-id")}`).getAttribute("full-polyline")
-      addPolyline(polyline)
     })
 
   }
