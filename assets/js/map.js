7b38c9c21a380092a4b7fd0688a5f24ba006b071
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-20 13:29:13 -0300

    [DIII-47] Added  map functions.

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,8 +3,25 @@
 // import 'leaflet/dist/leaflet';
 const polylineUtil = require('polyline-encoded');
 
+const map = L.map('map').setView([51.505, -0.09], 13);
+const exhibitionLayer = L.layerGroup();
+map.addLayer(exhibitionLayer);
+
+const defaultIcon = L.Icon.extend({
+  options: {
+    shadowUrl: "/images/markers/marker-shadow.png",
+    iconSize: [55, 55],
+    shadowSize: [49, 49],
+    shadowAnchor: [15, 45],
+    iconAnchor: [27, 53],
+    popupAnchor: [0, -50]
+  }
+})
+const unloadIcon = new defaultIcon({iconUrl: "/images/markers/marker-unload.png"})
+const loadIcon = new defaultIcon({iconUrl: "/images/markers/marker-shopper.png"})
+const driverIcon = new defaultIcon({iconUrl: "/images/markers/marker-driver.png"})
+
 export function initMap() {
-  const map = L.map('map').setView([51.505, -0.09], 13);
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -15,7 +32,26 @@ export function initMap() {
 }).addTo(map);
 }
 
-export function addPolyline(polyline) {
-  const map = L.map('map')
+export function addRoute(route) {
+  exhibitionLayer.clearLayers();
+  const polyline = addPolyline(route.full_polyline);
+  addMarker(route.driver_localization, "Driver", "<p>Driver</p>", driverIcon);
+  addMarker(route.to_collect_localization, "Ponto de Coleta", "<p>Ponto de Coleta</p>", loadIcon);
+  addMarker(route.to_deliver_localization, "Ponto de Entrega", "<p>Ponto de Entrega</p>", unloadIcon);
+  focusOn(polyline);
+}
 
+export function addMarker(coordinates, title, html, icon) {
+  let marker = L.marker(coordinates, {icon: icon}).addTo(exhibitionLayer);
+  marker.bindPopup(html);
+}
+
+function focusOn(poly) {
+  map.fitBounds(poly.getBounds());
+}
+
+export function addPolyline(polyline) {
+  const decodedPolyline = polylineUtil.decode(polyline)
+  const addedPoly = L.polyline(decodedPolyline, {color: 'red'}).addTo(exhibitionLayer);
+  return addedPoly;
 }
