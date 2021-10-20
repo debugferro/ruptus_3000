412974d06cf7f60ec4bf0bbc83848024c42738c6
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-20 13:28:57 -0300

    [DIII-47] Added report displayer hook event.

--- assets/js/report_displayer.js
+++ assets/js/report_displayer.js
@@ -1,12 +1,13 @@
+import { addRoute } from './map'
+
 export const ReportDisplayer = {
   mounted() {
     const hook = this;
     const selector = "#" + this.el.id;
     const element = this.el;
-    console.log(this.el, this.el.id);
 
     this.handleEvent("display-report", evt => {
-      console.log(evt, "FUI DISPATCHADO")
-    })
+      addRoute(evt.report)
+    });
   }
 }
