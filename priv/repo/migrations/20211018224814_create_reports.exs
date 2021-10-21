35cb5fb40a15b9f03a98462a5d97be1f354ca516
Gabriel Ferro <gabrielferroroque@hotmail.com>
2021-10-21 03:40:27 -0300

    Changed data type

--- priv/repo/migrations/20211018224814_create_reports.exs
+++ priv/repo/migrations/20211018224814_create_reports.exs
@@ -5,11 +5,11 @@ defmodule Ruptus3000.Repo.Migrations.CreateReports do
     create table(:reports) do
       add :to_collect_distance, :float
       add :to_collect_duration, :float
-      add :to_collect_polyline, :string
+      add :to_collect_polyline, :text
       add :to_deliver_distance, :float
       add :to_deliver_duration, :float
-      add :to_deliver_polyline, :string
-      add :full_polyline, :string
+      add :to_deliver_polyline, :text
+      add :full_polyline, :text
       add :to_collect_localization, {:array, :float}
       add :to_deliver_localization, {:array, :float}
       add :driver_localization, {:array, :float}
